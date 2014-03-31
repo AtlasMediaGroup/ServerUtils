43db3d7c843cdbdf7d4304e550f04aca9deaf5cc
ryan <ryan@ns3269875.ip-37-59-56.eu>
2014-04-01 00:12:08 +0200

    Added in the guts (Well some of it)

--- src/com/SuperiorNetworks/Projects/ServerUtils/ServerUtils.java
+++ src/com/SuperiorNetworks/Projects/ServerUtils/ServerUtils.java
@@ -1,11 +1,52 @@
 package com.SuperiorNetworks.Projects.ServerUtils;
 
+import com.SuperiorNetworks.Projects.ServerUtils.Commands.Command_serverutilshelp;
+import java.util.logging.Logger;
+import net.pravian.bukkitlib.BukkitLib;
+import net.pravian.bukkitlib.command.BukkitCommandHandler;
+import net.pravian.bukkitlib.config.YamlConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
-
-public class ServerUtils
+public class ServerUtils extends JavaPlugin
 {
-    
-    
-    
-    
+
+    public static final Logger logger = Bukkit.getLogger();
+    public static BukkitCommandHandler handler;
+    public static YamlConfig config;
+    public static ServerUtils plugin;
+
+    @Override
+    public void onDisable()
+    {
+        logger.info("Server Utilities Disabled");
+    }
+
+    @Override
+    public void onEnable()
+    {
+        BukkitLib.init(this);
+        plugin = this;
+        handler = new BukkitCommandHandler(plugin);
+        handler.setCommandLocation(Command_serverutilshelp.class.getPackage());
+        config = new YamlConfig(plugin, "config.yml", true);
+        config.load();
+        PluginDescriptionFile pdfFile = getDescription();
+        PluginManager pm = getServer().getPluginManager();
+        logger.info("{Server Utilities Enabled!");
+        config.options().copyDefaults(true);
+        saveConfig();
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+        return handler.handleCommand(sender, cmd, commandLabel, args);
+    }
+
 }
