1e15b9435cda4bfd3218b495e4d86f935f910b63
ryan <ryan@ns3269875.ip-37-59-56.eu>
2014-04-01 00:17:07 +0200

    Added the command in. Corrected it in the plugin.yml

--- src/com/SuperiorNetworks/Projects/ServerUtils/Commands/Command_serverutilshelp.java
+++ src/com/SuperiorNetworks/Projects/ServerUtils/Commands/Command_serverutilshelp.java
@@ -1,18 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.SuperiorNetworks.Projects.ServerUtils.Commands;
 
-/**
- *
- * @author ryan
- */
-public class Command_serverutilshelp
+import net.pravian.bukkitlib.command.BukkitCommand;
+import net.pravian.bukkitlib.command.CommandPermissions;
+import net.pravian.bukkitlib.command.SourceType;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(source = SourceType.PLAYER)
+public class Command_serverutilshelp extends BukkitCommand
 {
-    
-  
-    
+
+    @Override
+    public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
+    {
+        Player player = (Player) commandSender;
+
+        player.sendMessage("Sorry, We have not yet implimented this command. Please contact a Superior-Networks deceloper!");
+        return false;
+    }
 }
